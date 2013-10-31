2e905323c0e5d3a60d4b7b5def5f4057c1249f73
degarashi <slice013@gmail.com>
2013-10-31 20:27:49 +0900

    Dir: 内部型宣言をpublicに変更

--- dir.hpp
+++ dir.hpp
@@ -11,24 +11,26 @@
 namespace spn {
 	//! ディレクトリ管理
 	class Dir : public PathBlock {
-		using ToStrType = typename DirDep::ToStrType;
-		using ChType = typename DirDep::ChType;
-		using StrType = std::basic_string<ChType>;
-		struct PathReset {
-			const DirDep& dep;
-			std::string cwd;
+		public:
+			using ToStrType = typename DirDep::ToStrType;
+			using ChType = typename DirDep::ChType;
+			using StrType = std::basic_string<ChType>;
+		private:
+			struct PathReset {
+				const DirDep& dep;
+				std::string cwd;
 
-			PathReset(const DirDep& d): dep(d), cwd(dep.getcwd()) {}
-			~PathReset() { dep.chdir(cwd); }
-		};
+				PathReset(const DirDep& d): dep(d), cwd(dep.getcwd()) {}
+				~PathReset() { dep.chdir(cwd); }
+			};
 
-		using StrList = std::vector<std::string>;
-		using EnumCB = std::function<void (const PathBlock&)>;
-		using ModCB = std::function<bool (const PathBlock&, FStatus&)>;
-		DirDep	_dep;
-		void _enumEntryRegExR(const boost::regex& r, std::string& lpath, size_t baseLen, EnumCB cb) const;
-		void _enumEntryRegEx(const boost::regex& r, const std::string& path, EnumCB cb) const;
-		void _chmod(PathBlock& lpath, ModCB cb);
+			using StrList = std::vector<std::string>;
+			using EnumCB = std::function<void (const PathBlock&)>;
+			using ModCB = std::function<bool (const PathBlock&, FStatus&)>;
+			DirDep	_dep;
+			void _enumEntryRegExR(const boost::regex& r, std::string& lpath, size_t baseLen, EnumCB cb) const;
+			void _enumEntryRegEx(const boost::regex& r, const std::string& path, EnumCB cb) const;
+			void _chmod(PathBlock& lpath, ModCB cb);
 		public:
 			using PathBlock::PathBlock;
 			Dir(Dir&& d);
