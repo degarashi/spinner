c9ef4f5af5d19e5f73ae2ddfe19ef7d83add8b00
degarashi <slice013@gmail.com>
2013-12-05 20:10:43 +0900

    ArgHolder: rvalue reference渡しの修正

--- argholder.hpp
+++ argholder.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <type_traits>
+#include <cassert>
 
 namespace spn {
 	struct none_t{};
@@ -27,6 +28,8 @@ namespace spn {
 
 	template <class... Ts>
 	struct ArgHolder {
+		ArgHolder() = default;
+		ArgHolder(ArgHolder&& a) {}
 		template <class CB, class... TsA>
 		auto reverse(CB cb, TsA&&... tsa) -> decltype(cb(std::forward<TsA>(tsa)...)) {
 			return cb(std::forward<TsA>(tsa)...);
@@ -36,30 +39,62 @@ namespace spn {
 			return cb(std::forward<TsA>(tsa)...);
 		}
 	};
+	//! 引数を一旦とっておいて後で関数に渡す
+	/*! not reference, not pointerな型はrvalue_referenceで渡されるので2回以上呼べない */
 	template <class T0, class... Ts>
 	struct ArgHolder<T0,Ts...> {
-		template <class T>
-		struct Inner {
-			/*! T&& -> T&&
-			 *	T& -> T&
-			 *	T* -> T*
-			 *	T = const T& */
-			using type = typename std::conditional<!std::is_reference<T>::value  && !std::is_pointer<T>::value, const T&, T>::type;
-		};
-		using Type = typename Inner<T0>::type;
-		Type				_value;
+		T0		_value;
 		using Lower = ArgHolder<Ts...>;
-		Lower				_other;
+		Lower	_other;
+
+		// 2度呼び出しチェック
+		#ifdef DEBUG
+			bool	_bCall = false;
+			void _check() {
+				if(_bCall)
+					assert(!"Invalid ArgHolder call (called twice)");
+				_bCall = true;
+			}
+		#else
+			void _check() {}
+		#endif
+
+		template <class T2>
+		ArgHolder(ArgHolder<T2,Ts...>&& a): _value(std::move(a._value)), _other(std::move(a._other))
+		#ifdef DEBUG
+			, _bCall(a._bCall)
+		#endif
+		{}
 
 		template <class T0A, class... TsA>
 		ArgHolder(T0A&& t0, TsA&&... tsa): _value(std::forward<T0A>(t0)), _other(std::forward<TsA>(tsa)...) {}
-		template <class CB, class... TsA>
-		auto reverse(CB cb, TsA&&... tsa) -> decltype(_other.reverse(cb, std::forward<Type>(_value), std::forward<TsA>(tsa)...)) {
-			return _other.reverse(cb, std::forward<Type>(_value), std::forward<TsA>(tsa)...);
+
+		// T0がnot reference, not pointerな時はrvalue referenceで渡す
+		template <class CB, class... TsA,
+			class TT0 = T0, class = typename std::enable_if<std::is_object<TT0>::value>::type>
+		auto reverse(CB cb, TsA&&... tsa) -> decltype(_other.reverse(cb, std::move(_value), std::forward<TsA>(tsa)...)) {
+			_check();
+			return _other.reverse(cb, std::move(_value), std::forward<TsA>(tsa)...);
 		}
-		template <class CB, class... TsA>
-		auto inorder(CB cb, TsA&&... tsa) -> decltype(_other.inorder(cb, std::forward<TsA>(tsa)..., std::forward<Type>(_value))) {
-			return _other.inorder(cb, std::forward<TsA>(tsa)..., std::forward<Type>(_value));
+		// それ以外はそのまま渡す
+		template <class CB, class... TsA,
+			class TT0 = T0, class = typename std::enable_if<!std::is_object<TT0>::value>::type>
+		auto reverse(CB cb, TsA&&... tsa) -> decltype(_other.reverse(cb, _value, std::forward<TsA>(tsa)...)) {
+			return _other.reverse(cb, _value, std::forward<TsA>(tsa)...);
+		}
+
+		// T0がnot reference, not pointerな時はrvalue referenceで渡す
+		template <class CB, class... TsA,
+			class TT0 = T0, class = typename std::enable_if<std::is_object<TT0>::value>::type>
+		auto inorder(CB cb, TsA&&... tsa) -> decltype(_other.inorder(cb, std::forward<TsA>(tsa)..., std::move(_value))) {
+			_check();
+			return _other.inorder(cb, std::forward<TsA>(tsa)..., std::move(_value));
+		}
+		// それ以外はそのまま渡す
+		template <class CB, class... TsA,
+			class TT0 = T0, class = typename std::enable_if<!std::is_object<TT0>::value>::type>
+		auto inorder(CB cb, TsA&&... tsa) -> decltype(_other.inorder(cb, std::forward<TsA>(tsa)..., _value)) {
+			return _other.inorder(cb, std::forward<TsA>(tsa)..., _value);
 		}
 	};
 	//! 引数の順序を逆にしてコール
