cc5c5914de4bdcd3a0db5d5f0e7b3bdf339b5774
degarashi <slice013@gmail.com>
2014-03-29 00:35:31 +0900

    AdaptStd: unique_ptrでの初期化

--- adaptstd.cpp
+++ adaptstd.cpp
@@ -3,30 +3,32 @@
 namespace spn {
 	// ------------------ AdaptStd ------------------
 	const std::ios_base::seekdir AdaptStd::cs_flag[] = {std::istream::beg, std::istream::cur, std::istream::end};
-	AdaptStd::AdaptStd(std::istream& is): _is(is) {}
+	AdaptStd::AdaptStd(std::istream& is): _is(&is) {}
+	AdaptStd::AdaptStd(UP_Ist&& u): _up(std::move(u)), _is(_up.get()) {}
 	AdaptStd& AdaptStd::read(void* dst, streamsize len) {
-		_is.read(reinterpret_cast<char*>(dst), len);
+		_is->read(reinterpret_cast<char*>(dst), len);
 		return *this;
 	}
 	AdaptStd& AdaptStd::seekg(streamoff dist, Dir dir) {
-		_is.seekg(dist, cs_flag[dir]);
+		_is->seekg(dist, cs_flag[dir]);
 		return *this;
 	}
 	AdaptStream::streampos AdaptStd::tellg() const {
-		return _is.tellg();
+		return _is->tellg();
 	}
 	// ------------------ AdaptOStd ------------------
-	AdaptOStd::AdaptOStd(std::ostream& os): _os(os) {}
+	AdaptOStd::AdaptOStd(std::ostream& os): _os(&os) {}
+	AdaptOStd::AdaptOStd(UP_Ost&& u): _up(std::move(u)), _os(_up.get()) {}
 	AdaptOStd& AdaptOStd::write(const void* src, streamsize len) {
-		_os.write(reinterpret_cast<const char*>(src), len);
+		_os->write(reinterpret_cast<const char*>(src), len);
 		return *this;
 	}
 	AdaptOStd& AdaptOStd::seekp(streamoff dist, Dir dir) {
-		_os.seekp(dist, AdaptStd::cs_flag[dir]);
+		_os->seekp(dist, AdaptStd::cs_flag[dir]);
 		return *this;
 	}
 	AdaptOStream::streampos AdaptOStd::tellp() const {
-		return _os.tellp();
+		return _os->tellp();
 	}
 	// ------------------ AdaptIOStd ------------------
 	AdaptIOStd::AdaptIOStd(std::iostream& ios): AdaptStd(ios), AdaptOStd(ios) {}
--- adaptstream.hpp
+++ adaptstream.hpp
@@ -22,19 +22,27 @@ namespace spn {
 		virtual streampos tellp() const = 0;
 	};
 	struct AdaptStd : AdaptStream {
-		std::istream&	_is;
-
+		using UP_Ist = std::unique_ptr<std::istream>;
 		const static std::ios_base::seekdir cs_flag[];
+		UP_Ist			_up;
+		std::istream*	_is;
+
+		//! 参照だけ借りて初期化（開放しない）
 		AdaptStd(std::istream& is);
+		//! デストラクタで開放する
+		AdaptStd(UP_Ist&& u);
 		AdaptStd(const AdaptStd&) = default;
 		AdaptStd& read(void* dst, streamsize len) override;
 		AdaptStd& seekg(streamoff dist, Dir dir) override;
 		streampos tellg() const override;
 	};
 	struct AdaptOStd : AdaptOStream {
-		std::ostream&	_os;
+		using UP_Ost = std::unique_ptr<std::ostream>;
+		UP_Ost			_up;
+		std::ostream*	_os;
 
 		AdaptOStd(std::ostream& os);
+		AdaptOStd(UP_Ost&& u);
 		AdaptOStd(const AdaptOStd&) = default;
 		AdaptOStd& write(const void* src, streamsize len) override;
 		AdaptOStd& seekp(streamoff dist, Dir dir) override;
