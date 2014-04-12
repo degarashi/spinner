439327fec5003c18d3ab8186613836b26404c99a
degarashi <slice013@gmail.com>
2014-04-12 20:20:51 +0900

    グループ分けされた値を格納するOffsetIndex

--- misc.hpp
+++ misc.hpp
@@ -89,6 +89,46 @@ namespace spn {
 		}
 		return true;
 	}
+	//! グループ分けされた値を一本のvectorに収める
+	template <class T>
+	class OffsetIndex {
+		using Array = std::vector<T>;
+		using Offset = std::vector<int>;
+		using Pair = std::pair<const T*, const T*>;
+		Array		_value;		//! 全要素配列
+		Offset		_offset;	//! 各グループのオフセット(nGroup + 1)
+		T*			_pCur;
+
+		public:
+			OffsetIndex(int nReserveValue=0, int nReserveOffset=0):
+				_value(nReserveValue),
+				_offset(nReserveOffset)
+			{
+				clear();
+			}
+			void clear() {
+				_value.clear();
+				_offset.resize(1);
+				_offset[0] = 0;
+				_pCur = _value.data();
+			}
+			void pushValue(const T& val) {
+				_value.push_back(val);
+			}
+			//! 現在のグループを閉じて次のグループを開始
+			void finishGroup() {
+				_offset.push_back(_value.size());
+			}
+			int getNGroup() const { return _offset.size()-1; }
+			int getGroupSize(int n) const { return _offset[n+1]-_offset[n]; }
+			const Array& getArray() const { return _value; }
+			//! 特定のグループ範囲を取り出す
+			Pair getGroup(int n) const {
+				const int *pBegin = _value.data() + _offset[n],
+						*pEnd = _value.data() + _offset[n+1];
+				return std::make_pair(pBegin, pEnd);
+			}
+	};
 	//! 指定した順番で関数を呼び、クラスの初期化をする(マネージャの初期化用)
 	/*! デストラクタは初期化の時と逆順で呼ばれる */
 	template <class... Ts>
