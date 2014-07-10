670ad95c15096a7f7da293bb54857b9847b204d5
degarashi <slice013@gmail.com>
2014-07-10 18:03:12 +0900

    少しはマシなMatrixテストコードを追加

--- tests/math.cpp
+++ tests/math.cpp
@@ -13,23 +13,180 @@
 
 namespace spn {
 	namespace test {
+		#define MATRIXCHECK(z, dummy, dim)	MatrixCheck<BOOST_PP_TUPLE_ELEM(0,dim), BOOST_PP_TUPLE_ELEM(1,dim)>();
+		namespace {
+			constexpr float Threshold = 5e-4f,
+							ValueMin = -1e4f,
+							ValueMax = 1e4f,
+							N_Iteration = 100;
+			auto OpMul = [](auto& v0, auto& v1){ return v0 * v1; };
+			auto OpDiv = [](auto& v0, auto& v1){ return v0 / v1; };
+			auto OpAdd = [](auto& v0, auto& v1){ return v0 + v1; };
+			auto OpSub = [](auto& v0, auto& v1){ return v0 - v1; };
+			auto OpCopy = [](auto& v0, auto& v1){ return v1; };
+			template <int M, int N>
+			using Array2D = std::array<std::array<float, N>, M>;
+			template <class RD, class AR, int M, int N, bool A>
+			void SetRandom(RD& rd, AR& ar, MatT<M,N,A>& m) {
+				for(int i=0 ; i<M ; i++) {
+					for(int j=0 ; j<N ; j++) {
+						m.ma[i][j] = ar[i][j] = rd();
+					}
+				}
+			}
+			template <class AR, int M, int N, bool A>
+			bool Compare(const AR& ar, const MatT<M,N,A>& m) {
+				for(int i=0 ; i<M ; i++) {
+					for(int j=0 ; j<N ; j++) {
+						auto diff = ar[i][j] - m.ma[i][j];
+						if(std::fabs(diff) >= Threshold)
+							return false;
+					}
+				}
+				return true;
+			}
+			template <class AR>
+			struct GetArraySize {
+				template <class T, size_t N>
+				constexpr static int _size(const std::array<T,N>*) {
+					return N;
+				}
+				constexpr static int size() {
+					constexpr AR* ar = nullptr;
+					return _size(ar);
+				}
+			};
+			template <class AR, class OP>
+			void Operate(AR& ar, OP&& op) {
+				using Type = std::decay_t<decltype(ar[0])>;
+				for(int i=0 ; i<GetArraySize<AR>::size() ; i++) {
+					for(int j=0 ; j<GetArraySize<Type>::size() ; j++)
+						op(ar[i][j]);
+				}
+			}
+			template <class AR, class OP>
+			void Operate1(AR& ar0, const AR& ar1, OP&& op) {
+				using Type = std::decay_t<decltype(ar0[0])>;
+				for(int i=0 ; i<GetArraySize<AR>::size(); i++) {
+					for(int j=0 ; j<GetArraySize<Type>::size() ; j++)
+						ar0[i][j] = op(ar0[i][j], ar1[i][j]);
+				}
+			}
+			template <class AR, class OP>
+			void MulArray(AR& ar0, const AR& ar1, OP&& op) {
+				using Type = std::decay_t<decltype(ar0[0])>;
+				constexpr int width{ GetArraySize<AR>::size() },
+							height{ GetArraySize<Type>::size() };
+				static_assert(width == height, "rectangular matrix only");
+				AR tmp;
+				for(int i=0 ; i<width ; i++) {
+					for(int j=0 ; j<width; j++) {
+						tmp[i][j] = 0;
+						for(int k=0 ; k<width ; k++)
+							tmp[i][j] += ar0[i][k] * ar1[k][j];
+					}
+				}
+				for(int i=0 ; i<width ; i++) {
+					for(int j=0 ; j<width ; j++)
+						ar0[i][j] = tmp[i][j];
+				}
+			}
+			template <class AR>
+			void TransposeArray(AR& ar0, const AR& ar1) {
+				using Type = std::decay_t<decltype(ar0[0])>;
+				constexpr int width{ GetArraySize<AR>::size() };
+				static_assert(width == GetArraySize<Type>::size(), "width must be equal to height");
+				for(int i=0 ; i<width ; i++) {
+					for(int j=0 ; j<width ; j++)
+						ar0[i][j] = ar1[j][i];
+				}
+			}
+			template <class RD, int M, int N, bool A>
+			void _MatrixCheckRect(RD& rd,
+									std::integral_constant<int,M>,
+									std::integral_constant<int,N>,
+									std::integral_constant<bool,A>)
+			{}
+			template <class RD, int N, bool A>
+			void _MatrixCheckRect(RD& rd,
+									std::integral_constant<int,N>,
+									std::integral_constant<int,N>,
+									std::integral_constant<bool,A>)
+			{
+				using Mat = MatT<N,N,A>;
+				auto fnTestMat = [&rd](auto&& opA, auto&& opM) {
+					for(int i=0 ; i<N_Iteration ; i++) {
+						Mat m0, m1;
+						Array2D<N,N> ar0, ar1;
+						SetRandom(rd, ar0, m0);
+						SetRandom(rd, ar1, m1);
+						opA(ar0, ar1, std::forward<std::decay_t<decltype(opM)>>(opM));
+						auto m2 = opM(m0,m1);
+						m0 = opM(m0, m1);
+						Assert(Trap, Compare(ar0, m0))
+					}
+				};
+				// + mat
+				fnTestMat([](auto& a0, auto& a1, auto&&){ Operate1(a0,a1,OpAdd); }, OpAdd);
+				// - mat
+				fnTestMat([](auto& a0, auto& a1, auto&&){ Operate1(a0,a1,OpSub); }, OpSub);
+				// * mat
+				auto* ptr = &MulArray<std::array<std::array<float, N>, N>, decltype(OpMul)>;
+				fnTestMat(ptr, OpMul);
+				// = mat
+				fnTestMat([](auto& a0, auto& a1, auto&&){ Operate1(a0,a1,OpCopy); }, OpCopy);
+				// determinant
+				// inversion
+
+				// transpose
+				Array2D<N,N>	ar0,ar1;
+				Mat				m0,m1;
+				SetRandom(rd, ar1, m1);
+				TransposeArray(ar0, ar1);
+				m0 = m1.transposition();
+				Assert(Trap, Compare(ar0, m0))
+			}
+			template <int M, int N, bool A>
+			void _MatrixCheck() {
+				// 配列を使って計算した結果とMatrixクラスの結果を、誤差含めて比較
+				auto rd = mgr_random.get(0);
+				auto rdF = [&rd](){ return rd.template getUniformRange<float>(ValueMin, ValueMax); };
+
+				using Mat = MatT<M,N,A>;
+				auto fnTestScalar = [&rdF](auto& rd, auto&& op) {
+					for(int i=0 ; i<N_Iteration ; i++) {
+						Mat m0;
+						Array2D<M,N> ar0;
+						SetRandom(rdF, ar0, m0);
+
+						float value = rd();
+						Operate(ar0, [value, &op](auto& v){ v = op(v, value); });
+						m0 = op(m0, value);
+						Assert(Trap, Compare(ar0, m0))
+					}
+				};
+				// += scalar
+				fnTestScalar(rdF, OpAdd);
+				// -= scalar
+				fnTestScalar(rdF, OpSub);
+				// *= scalar
+				fnTestScalar(rdF, OpMul);
+				// /= scalar
+				fnTestScalar(rdF, OpDiv);
+				// --- Rect Only ---
+				_MatrixCheckRect(rdF,
+								std::integral_constant<int,M>(),
+								std::integral_constant<int,N>(),
+								std::integral_constant<bool,A>());
+			}
+			template <int M, int N>
+			void MatrixCheck() {
+				_MatrixCheck<M,N,false>();
+				_MatrixCheck<M,N,true>();
+			}
+		}
 		void MatTest() {
-			Mat44 m4 = Mat44::RotationZ(DEGtoRAD(0));
-			auto idx = Mat44(Mat44::TagIdentity);
-			auto mm4 = m4 * idx;
-			m4 *= idx;
-
-			AMat43 mrY = AMat43::RotationY(DEGtoRAD(45));
-			auto mrY4 = mrY.convertA44();
-
-			AMat33 mrX = AMat33::RotationX(DEGtoRAD(90));
-			AVec3 vrot(0,0,100);
-			auto vr = vrot * mrX;
-			float len = vrot.length();
-
-			using MyStack = MatStack<Mat33, MatStackTag::PushRight>;
-			MyStack stk;
-			stk.push(Mat33(mrX));
+			BOOST_PP_SEQ_FOR_EACH(MATRIXCHECK, BOOST_PP_IDENTITY, SEQ_MATDEF)
 		}
 		void PoseTest() {
 			Pose2D p2(AVec2(100,200), 128, AVec2(1,1)),
