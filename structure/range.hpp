fcbaa105a8ab5653791fc009d69e527270554729
degarashi <slice013@gmail.com>
2014-10-07 18:26:47 +0900

    Range構造体を追加

---
+++ structure/range.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "error.hpp"
+
+namespace spn {
+	template <class T>
+	struct Range {
+		T	from,
+			to;
+		Range() = default;
+		Range(const T& f, const T& t): from(f), to(t) {
+			AssertP(Trap, from <= to, "invalid range");
+		}
+	};
+	using RangeI = Range<int>;
+	using RangeF = Range<float>;
+}
+
