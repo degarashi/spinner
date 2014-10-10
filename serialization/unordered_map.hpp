2a2a6549e0df66c3512d8ad11547956be1f1b84e
degarashi <slice013@gmail.com>
2014-10-10 21:02:07 +0900

    serialization/unordered_map: 型指定のバグ修正

--- serialization/unordered_map.hpp
+++ serialization/unordered_map.hpp
@@ -12,16 +12,18 @@ namespace boost {
 		void load(Archive& ar, std::unordered_map<Key,T,Hash,KeyEqual,Allocator>& um, const unsigned int /*ver*/) {
 			um.clear();
 
+			using UM = std::decay_t<decltype(um)>;
 			// 一度pointer listに変換
-			std::vector<std::pair<Key,T>*> ptr;
+			std::vector<typename UM::value_type*> ptr;
 			ar & boost::serialization::make_nvp("data", ptr);
 			for(auto* p : ptr)
 				um.emplace(p->first, std::move(p->second));
 		}
 		template <class Archive, class Key, class T, class Hash, class KeyEqual, class Allocator>
 		void save(Archive& ar, const std::unordered_map<Key,T,Hash,KeyEqual,Allocator>& um, const unsigned int /*ver*/) {
+			using UM = std::decay_t<decltype(um)>;
 			// 一度pointer listに変換
-			std::vector<std::pair<Key,T>*> ptr;
+			std::vector<const typename UM::value_type*> ptr;
 			for(auto& p : um)
 				ptr.push_back(&p);
 			ar & boost::serialization::make_nvp("data", ptr);
