f1ebb4f76bd3ac87d7e00ebf954a57d08e0ffca8
degarashi <slice013@gmail.com>
2014-10-16 14:50:00 +0900

    AndroidのNDKパス, BoostLibraryパスは環境変数で設定するようにした

--- Android.cmake
+++ Android.cmake
@@ -1,10 +1,36 @@
 set(UNIX 1)
 add_definitions(-DUNIX)
 
-set(BOOST_PATH "/opt/boost/${ANDROID_ARCH_LONG}")
+# 環境変数のチェックとディレクトリの存在確認
+set(BOOST_PATH $ENV{BOOST_PATH})
+set(ANDROID_NDK_ROOT $ENV{ANDROID_NDK_ROOT})
+set(ANDROID_VER $ENV{ANDROID_VER})
+
+message(STATUS ">>-- enviroment variables check --<<")
+message(STATUS "BOOST_PATH: ${BOOST_PATH}")
+message(STATUS "ANDROID_NDK_ROOT: ${ANDROID_NDK_ROOT}")
+message(STATUS "ANDROID_VER: ${ANDROID_VER}")
+if((NOT IS_DIRECTORY ${BOOST_PATH})
+	OR (NOT IS_DIRECTORY ${ANDROID_NDK_ROOT})
+	OR (NOT DEFINED ANDROID_VER)
+	OR ("${ANDROID_VER}" LESS "1"))
+	message(FATAL_ERROR "
+		Some required enviroment values are not defined or invalid.
+		Example:
+			BOOST_PATH = /opt/boost/arm
+			ANDROID_NDK_ROOT = /opt/android-ndk
+			ANDROID_VER = 10")
+endif()
+message(STATUS ">>-- enviroment variables ok --<<")
+
+# Boost library ディレクトリが存在するか確認
+set(BOOST_PATH "${BOOST_PATH}/${ANDROID_ARCH_LONG}")
+message(STATUS "BOOST_PATH(actual): ${BOOST_PATH}")
+if(NOT IS_DIRECTORY ${BOOST_PATH})
+	message(FATAL_ERROR "${BOOST_PATH} is not exist.")
+endif()
+
 set(ANDROID_PLATFORM "linux-x86_64")
-set(ANDROID_NDK_ROOT "/opt/adt-bundle-${ANDROID_PLATFORM}/ndk")
-set(ANDROID_VER "9")
 set(TOOL_PREFIX ${ANDROID_ARCH_LONG})
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --sysroot=${ANDROID_NDK_ROOT}/platforms/android-${ANDROID_VER}/arch-${ANDROID_ARCH_SHORT}")
