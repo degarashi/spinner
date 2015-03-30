8a3437bc0049b9ef16d33ca93dd3e46e3387e5e4
degarashi <slice013@gmail.com>
2015-03-30 16:07:16 +0900

    Range: 範囲チェック関数など追加

--- structure/range.hpp
+++ structure/range.hpp
@@ -10,6 +10,23 @@ namespace spn {
 		constexpr Range(const T& f, const T& t): from(f), to(t) {
 			AssertP(Trap, from <= to, "invalid range");
 		}
+		//! 値の範囲チェック
+		bool hit(const T& v) const {
+			return v >= from
+				&& v <= to;
+		}
+		//! 範囲が重なっているか
+		bool hit(const Range& r) const {
+			return !(to < r.from
+					&& from > r.to);
+		}
+		bool operator == (const Range& r) const {
+			return from == r.from
+				&& to == r.to;
+		}
+		bool operator != (const Range& r) const {
+			return !(this->operator == (r));
+		}
 	};
 	using RangeI = Range<int>;
 	using RangeF = Range<float>;
