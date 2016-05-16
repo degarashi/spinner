db07f0671214f67f9722eb697ce2b2dbd9705ae3
degarashi <slice013@gmail.com>
2016-05-16 14:04:23 +0900

    コード整形

--- unituple/operator.hpp
+++ unituple/operator.hpp
@@ -13,62 +13,88 @@ namespace spn {
 	template <class T>
 	struct IsTupleBased : decltype(IsConvertibleToTuple((T*)nullptr)) {};
 	//! ベースのstd::tuple型を取り出す
+	//! std::tupleならそのまま返す
 	template <class... Ts>
 	auto GetTuple(std::tuple<Ts...>*) -> std::tuple<Ts...>;
+	// メンバにTupleを持っていればそれを返す
 	template <class T>
 	auto GetTuple(T* ptr, typename T::Tuple* tag=nullptr) -> decltype(GetTuple(tag));
 
+	template <class CB, class... Ts>
+	void TupleForEach(CB&& cb, Ts&&... ts);
 	namespace detail {
 		template <class OP, class A0, class... Args>
-		decltype(auto) TupleForEach(OP&& /*op*/, std::integral_constant<int,0>, A0&& a0, Args&&... /*args*/);
+		void TupleForEach(OP&& op, std::integral_constant<int,0>, A0&& a0, Args&&... args);
 		template <class OP, int N, class... Args>
-		decltype(auto) TupleForEach(OP&& op, std::integral_constant<int,N>, Args&&... args);
+		void TupleForEach(OP&& op, std::integral_constant<int,N>, Args&&... args);
 
-		template <class OP, class... Ts,
+		static void* Enabler;
+		//! 与えられた引数でコールバック関数を呼ぶ
+		template <class CB, class... Ts,
 					// 何れもstd::tuple型ではない
-					class = std::enable_if_t<
+					typename std::enable_if_t<
 						And_t<
 							(!IsTupleBased<
 								std::decay_t<Ts>
 							>::value)...
 						>::value
-					>
+					>*& = Enabler
 				>
-		void CallOP(OP&& op, Ts&&... ts) {
-			op(std::forward<Ts>(ts)...);
+		void CallCB(CB&& cb, Ts&&... ts) {
+			cb(std::forward<Ts>(ts)...);
 		}
-		template <class OP, class... Ts,
+		template <class CB, class... Ts,
 					// 全てstd::tuple型
-					class = std::enable_if_t<
+					typename std::enable_if_t<
 								And_t<
 									IsTupleBased<
 										std::decay_t<Ts>
 									>::value...
-								>::value>
-					>
-		decltype(auto) CallOP(OP&& op, Ts&&... ts) {
-			using T = typename FirstType<std::decay_t<Ts>...>::type;
-			using Tuple = decltype(GetTuple((T*)nullptr));
-			constexpr int sz = std::tuple_size<Tuple>::value;
-			return ::spn::detail::TupleForEach(op, std::integral_constant<int,sz>(), std::forward<Ts>(ts)...);
+								>::value
+					>*& = Enabler
+				>
+		void CallCB(CB&& cb, Ts&&... ts) {
+			::spn::TupleForEach(cb, std::forward<Ts>(ts)...);
 		}
 
+		// 終端処理
 		template <class OP, class A0, class... Args>
-		decltype(auto) TupleForEach(OP&& /*op*/, std::integral_constant<int,0>, A0&& a0, Args&&... /*args*/) {
-			return std::forward<A0>(a0);
-		}
-		template <class OP, int N, class... Args>
-		decltype(auto) TupleForEach(OP&& op, std::integral_constant<int,N>, Args&&... args) {
-			CallOP(op, std::get<N-1>((decltype(GetTuple((std::decay_t<Args>*)nullptr))&)args)...);
-			return TupleForEach(op, std::integral_constant<int,N-1>(), std::forward<Args>(args)...);
+		void TupleForEach(OP&& /*op*/, std::integral_constant<int,0>, A0&& /*a0*/, Args&&... /*args*/) {}
+		/*!
+			\tparam	CB		コールバック関数
+			\tparam N		処理をするTupleの位置
+			\tparam Args	Tuple列
+		*/
+		template <class CB, int N, class... Args>
+		void TupleForEach(CB&& cb, std::integral_constant<int,N>, Args&&... args) {
+			CallCB(
+				cb,
+				std::get<N-1>(
+					(decltype(GetTuple((std::decay_t<Args>*)nullptr))&)args
+				)...
+			);
+			TupleForEach(
+				cb,
+				std::integral_constant<int,N-1>(),
+				std::forward<Args>(args)...
+			);
 		}
 	}
-	template <class OP, class... Ts>
-	decltype(auto) TupleForEach(OP&& op, Ts&&... ts) {
+	/*!
+		Tupleのサイズを計算して内部関数に受け渡す
+		\tparam CB	コールバック関数
+		\tparam Ts	Tuple列
+	*/
+	template <class CB, class... Ts>
+	void TupleForEach(CB&& cb, Ts&&... ts) {
 		using T = typename FirstType<std::decay_t<Ts>...>::type;
 		using Tuple = decltype(GetTuple((T*)nullptr));
 		constexpr int sz = std::tuple_size<Tuple>::value;
-		return detail::TupleForEach(op, std::integral_constant<int,sz>(), std::forward<Ts>(ts)...);
+		detail::TupleForEach(
+			cb,
+			std::integral_constant<int,sz>(),
+			std::forward<Ts>(ts)...
+		);
 	}
 
 	static void* Enabler2;
